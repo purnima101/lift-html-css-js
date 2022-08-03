91aae3b49efaba5ae8b6de1230245dc9b03c1d84
purnima101 <purnimaagarwal2013@gmail.com>
2022-08-03 19:08:51 +0530

    ok

---
+++ index.js
@@ -0,0 +1,16 @@
+let lift=document.getElementById("lift")
+let up=document.getElementById("up")
+let down=document.getElementById("down")
+
+let key0=document.getElementById("f0")
+let key1=document.getElementById("f1")
+let key2=document.getElementById("f2")
+let key3=document.getElementById("f3")
+let key4=document.getElementById("f4")
+let key5=document.getElementById("f5")
+let key6=document.getElementById("f6")
+let key7=document.getElementById("f7")
+let key8=document.getElementById("f8")
+let key9=document.getElementById("f9")
+
+
