7ba52ddf8a4df0e9d506c4a3a9da86c7d7ee32fb
purnima101 <purnimaagarwal2013@gmail.com>
2022-08-04 19:39:58 +0530

    ok

--- index.js
+++ index.js
@@ -13,4 +13,55 @@ let key7=document.getElementById("f7")
 let key8=document.getElementById("f8")
 let key9=document.getElementById("f9")
 
+let state=1
+
+
+let lpos=0
+let upArr=[]
+let downArr=[]
+
+function move(floor)
+{
+    
+    // console.log(lpos)
+    // console.log(floor)
+    if(floor>lpos)
+    {
+        state=1
+
+        upArr.push(floor)
+        upArr.sort()
+        let x=upArr[0]*10 +"%"
+        console.log(upArr)
+        upArr.shift()
+        lift.style.bottom=x
+        lpos=floor
+        
+
+    }
+    else if(floor<lpos)
+    {
+       state=0
+        downArr.push(floor)
+        downArr.sort()
+        let x=downArr[0]*10 +"%"
+        console.log(downArr)
+        downArr.shift()
+
+        lift.style.bottom=x
+        lpos=floor
+        
+    }
+
+    
+    
+
+   
+  
+}
+
+
+
+
+
 
